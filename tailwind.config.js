99d7d15f54625600953accd8e6cd328af77fd422
jgl3hb <lewis.jeffreyg@gmail.com>
2023-11-15 11:15:42 -0500

    redirects file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
-// tailwind.config.js
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // paths to all template files
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  // Remove the darkMode line if you don't need dark mode,
+  // or set it to 'media' or 'class' based on your preference
+  darkMode: 'media', // or 'class'
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-};
+}
